09cf55d53da52c4471a0aab02d6563b66a774299
tartarus-git <niktedig@gmail.com>
2024-06-30 21:13:21 +0200

    Made the quine.c code nicer. Also added hash checking in the quine_loop script so that it will tell us when the quine isn't producing the right output.

--- quine.c
+++ quine.c
@@ -6,17 +6,27 @@ const char * const this_source_code = "#include <stdlib.h>\n\
 \n\
 const char * const this_source_code = \"??\";\n\
 \n\
-void print_quine_output(const char * const output_template) {\n\
-	for (const char *ptr = output_template; *ptr != '\\0'; ptr++) {\n\
-		if (*ptr == '?' && *(ptr + 1) == '?') {\n\
-			for (const char *inner_ptr = output_template; *inner_ptr != '\\0'; inner_ptr++) {\n\
-				if (*inner_ptr == '\\n') { printf(\"\\\\n\\\\\"); }\n\
-				else if (*inner_ptr == '\\\\' || *inner_ptr == '\\\"') { printf(\"\\\\\"); }\n\
+void print_quine_output(const char * const output_template)\n\
+{\n\
+	for (const char *ptr = output_template; *ptr != '\\0'; ptr++)\n\
+	{\n\
+		if (*ptr == '?' && *(ptr + 1) == '?')\n\
+		{\n\
+			for (const char *inner_ptr = output_template; *inner_ptr != '\\0'; inner_ptr++)\n\
+			{\n\
+				switch (*inner_ptr) {\n\
+					case '\\n': printf(\"\\\\n\\\\\"); break;\n\
+					case '\\\\': case '\\\"': printf(\"\\\\\"); break;\n\
+					// default: break;\n\
+				}\n\
 				printf(\"%c\", *inner_ptr);\n\
+\n\
 			}\n\
+\n\
 			ptr++;\n\
 			continue;\n\
 		}\n\
+\n\
 		printf(\"%c\", *ptr);\n\
 	}\n\
 }\n\
@@ -27,17 +37,27 @@ int main(void) {\n\
 }\n\
 ";
 
-void print_quine_output(const char * const output_template) {
-	for (const char *ptr = output_template; *ptr != '\0'; ptr++) {
-		if (*ptr == '?' && *(ptr + 1) == '?') {
-			for (const char *inner_ptr = output_template; *inner_ptr != '\0'; inner_ptr++) {
-				if (*inner_ptr == '\n') { printf("\\n\\"); }
-				else if (*inner_ptr == '\\' || *inner_ptr == '\"') { printf("\\"); }
+void print_quine_output(const char * const output_template)
+{
+	for (const char *ptr = output_template; *ptr != '\0'; ptr++)
+	{
+		if (*ptr == '?' && *(ptr + 1) == '?')
+		{
+			for (const char *inner_ptr = output_template; *inner_ptr != '\0'; inner_ptr++)
+			{
+				switch (*inner_ptr) {
+					case '\n': printf("\\n\\"); break;
+					case '\\': case '\"': printf("\\"); break;
+					// default: break;
+				}
 				printf("%c", *inner_ptr);
+
 			}
+
 			ptr++;
 			continue;
 		}
+
 		printf("%c", *ptr);
 	}
 }
