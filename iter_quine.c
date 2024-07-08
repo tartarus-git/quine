5e2a29d6d2fd42bb8eb5c6a2a0efa98f2145381e
tartarus-git <niktedig@gmail.com>
2024-07-08 14:24:58 +0200

    Added iter_quine.c, a quine that counts the iterations that you put it through in a comment.

---
+++ iter_quine.c
@@ -0,0 +1,86 @@
+// QUINE ITERATION: 0
+
+#include <stdlib.h>
+#include <stdio.h>
+
+const char * const this_source_code = "//QUINE ITERATION: @@\n\
+\n\
+#include <stdlib.h>\n\
+#include <stdio.h>\n\
+\n\
+const char * const this_source_code = \"??\";\n\
+\n\
+void print_quine_output(const char * const output_template)\n\
+{\n\
+	for (const char *ptr = output_template; *ptr != '\\0'; ptr++)\n\
+	{\n\
+		if (*ptr == '?' && *(ptr + 1) == '?')\n\
+		{\n\
+			for (const char *inner_ptr = output_template; *inner_ptr != '\\0'; inner_ptr++)\n\
+			{\n\
+				switch (*inner_ptr) {\n\
+					case '\\n': printf(\"\\\\n\\\\\"); break;\n\
+					case '\\\\': case '\\\"': printf(\"\\\\\"); break;\n\
+					// default: break;\n\
+				}\n\
+				printf(\"%c\", *inner_ptr);\n\
+\n\
+			}\n\
+\n\
+			ptr++;\n\
+			continue;\n\
+		}\n\
+\n\
+		if (*ptr == '@' && *(ptr + 1) == '@')\n\
+		{\n\
+			printf(\"%u\", @@ + 1);\n\
+			ptr++;\n\
+			continue;\n\
+		}\n\
+\n\
+		printf(\"%c\", *ptr);\n\
+	}\n\
+}\n\
+\n\
+int main(void) {\n\
+	print_quine_output(this_source_code);\n\
+	return EXIT_SUCCESS;\n\
+}\n\
+";
+
+void print_quine_output(const char * const output_template)
+{
+	for (const char *ptr = output_template; *ptr != '\0'; ptr++)
+	{
+		if (*ptr == '?' && *(ptr + 1) == '?')
+		{
+			for (const char *inner_ptr = output_template; *inner_ptr != '\0'; inner_ptr++)
+			{
+				switch (*inner_ptr) {
+					case '\n': printf("\\n\\"); break;
+					case '\\': case '\"': printf("\\"); break;
+					// default: break;
+				}
+				printf("%c", *inner_ptr);
+
+			}
+
+			ptr++;
+			continue;
+		}
+
+		if (*ptr == '@' && *(ptr + 1) == '@')
+		{
+			printf("%u", 0 + 1);
+			ptr++;
+			continue;
+		}
+
+		printf("%c", *ptr);
+	}
+}
+
+int main(void) {
+	print_quine_output(this_source_code);
+	return EXIT_SUCCESS;
+}
