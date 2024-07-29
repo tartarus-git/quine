42e139d379ef737aea11b5a065eb14db6ef46a89
tartarus-git <niktedig@gmail.com>
2024-07-29 10:04:25 +0200

    dual_iter_quine.c works now, and it's kind of beautiful. Also added dual_iter_quine_loop. Also updated .gitignore with the relevant files.

--- dual_iter_quine.c
+++ dual_iter_quine.c
@@ -3,7 +3,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-const char * const c_source = "//QUINE ITERATION: @@\n\
+const char * const c_source = "// QUINE ITERATION: @@\n\
 \n\
 #include <stdlib.h>\n\
 #include <stdio.h>\n\
@@ -12,39 +12,33 @@ const char * const c_source = \"??\";\n\
 \n\
 const char * const py_source = \"^^\";\n\
 \n\
+void print_quine_substitution(const char * const source)\n\
+{\n\
+	for (const char *ptr = source; *ptr != '\\0'; ptr++)\n\
+	{\n\
+		switch (*ptr) {\n\
+			case '\\n': printf(\"\\\\n\\\\\"); break;\n\
+			case '\\\\': case '\\\"': putchar('\\\\'); break;\n\
+			// default: break;\n\
+		}\n\
+		putchar(*ptr);\n\
+	}\n\
+}\n\
+\n\
 int main(void)\n\
 {\n\
 	for (const char *ptr = py_source; *ptr != '\\0'; ptr++)\n\
 	{\n\
 		if (*ptr == '?' && *(ptr + 1) == '?')\n\
 		{\n\
-			for (const char *inner_ptr = c_source; *inner_ptr != '\\0'; inner_ptr++)\n\
-			{\n\
-				switch (*inner_ptr) {\n\
-					case '\\n': printf(\"\\\\n\\\\\"); break;\n\
-					case '\\\\': case '\\\"': printf(\"\\\\\"); break;\n\
-					// default: break;\n\
-				}\n\
-				printf(\"%c\", *inner_ptr);\n\
-\n\
-			}\n\
-\n\
+			print_quine_substitution(c_source);\n\
 			ptr++;\n\
 			continue;\n\
 		}\n\
 \n\
 		if (*ptr == '^' && *(ptr + 1) == '^')\n\
 		{\n\
-			for (const char *inner_ptr = py_source; *inner_ptr != '\\0'; inner_ptr++)\n\
-			{\n\
-				switch (*inner_ptr) {\n\
-					case '\\n': printf(\"\\\\n\\\\\"); break;\n\
-					case '\\\\': case '\\\"': printf(\"\\\\\"); break;\n\
-					// default: break;\n\
-				}\n\
-				printf(\"%c\", *inner_ptr);\n\
-			}\n\
-\n\
+			print_quine_substitution(py_source);\n\
 			ptr++;\n\
 			continue;\n\
 		}\n\
@@ -56,19 +50,41 @@ int main(void)\n\
 			continue;\n\
 		}\n\
 \n\
-		printf(\"%c\", *ptr);\n\
+		putchar(*ptr);\n\
 	}\n\
 \n\
+	putchar('\\n');\n\
+\n\
 	return EXIT_SUCCESS;\n\
-}\n\
-";
+}";
 
 const char * const py_source = "# QUINE ITERATION: @@\n\
 \n\
 c_source = \"??\"\n\
+\n\
 py_source = \"^^\"\n\
-print(c_source.replace(\"\\100\\100\", str(@@ + 1)).replace(\"\\077\\077\", c_source.replace(\"\\\\\", \"\\\\\\\\\").replace(\"\\\"\", \"\\\\\\\"\").replace(\"\\n\", \"\\\\n\\\\\\n\")).replace(\"\\136\\136\", py_source.replace(\"\\\\\", \"\\\\\\\\\").replace(\"\\\"\", \"\\\\\\\"\").replace(\"\\n\", \"\\\\n\\\\\\n\")))\n\
-";
+\n\
+def create_quine_substitution(source):\n\
+	return source.replace(\"\\\\\", \"\\\\\\\\\") \\\n\
+		     .replace(\"\\\"\", \"\\\\\\\"\") \\\n\
+		     .replace(\"\\n\", \"\\\\n\\\\\\n\")\n\
+\n\
+print(c_source.replace(\"\\100\\100\", str(@@ + 1)) \\\n\
+	      .replace(\"\\136\\136\", create_quine_substitution(py_source)) \\\n\
+	      .replace(\"\\077\\077\", create_quine_substitution(c_source), 1))";
+
+void print_quine_substitution(const char * const source)
+{
+	for (const char *ptr = source; *ptr != '\0'; ptr++)
+	{
+		switch (*ptr) {
+			case '\n': printf("\\n\\"); break;
+			case '\\': case '\"': putchar('\\'); break;
+			// default: break;
+		}
+		putchar(*ptr);
+	}
+}
 
 int main(void)
 {
@@ -76,32 +92,14 @@ int main(void)
 	{
 		if (*ptr == '?' && *(ptr + 1) == '?')
 		{
-			for (const char *inner_ptr = c_source; *inner_ptr != '\0'; inner_ptr++)
-			{
-				switch (*inner_ptr) {
-					case '\n': printf("\\n\\"); break;
-					case '\\': case '\"': printf("\\"); break;
-					// default: break;
-				}
-				printf("%c", *inner_ptr);
-			}
-
+			print_quine_substitution(c_source);
 			ptr++;
 			continue;
 		}
 
 		if (*ptr == '^' && *(ptr + 1) == '^')
 		{
-			for (const char *inner_ptr = py_source; *inner_ptr != '\0'; inner_ptr++)
-			{
-				switch (*inner_ptr) {
-					case '\n': printf("\\n\\"); break;
-					case '\\': case '\"': printf("\\"); break;
-					// default: break;
-				}
-				printf("%c", *inner_ptr);
-			}
-
+			print_quine_substitution(py_source);
 			ptr++;
 			continue;
 		}
@@ -113,8 +111,10 @@ int main(void)
 			continue;
 		}
 
-		printf("%c", *ptr);
+		putchar(*ptr);
 	}
+
+	putchar('\n');
 
 	return EXIT_SUCCESS;
 }
