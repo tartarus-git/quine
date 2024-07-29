5bc93c4483a149cc6bd71c9e86841c03f7898322
tartarus-git <niktedig@gmail.com>
2024-07-29 12:22:05 +0200

    modified py_quine slightly, added py_quine_loop, added relevant files to .gitignore.

--- py_quine.py
+++ py_quine.py
@@ -1,4 +1,3 @@
 this_source_code = "this_source_code = \"??\"\n\
-print(this_source_code.replace(\"\\077\\077\", this_source_code.replace(\"\\\\\", \"\\\\\\\\\").replace(\"\\\"\", \"\\\\\\\"\").replace(\"\\n\", \"\\\\n\\\\\\n\")))\n\
-"
+print(this_source_code.replace(\"\\077\\077\", this_source_code.replace(\"\\\\\", \"\\\\\\\\\").replace(\"\\\"\", \"\\\\\\\"\").replace(\"\\n\", \"\\\\n\\\\\\n\")))"
 print(this_source_code.replace("\077\077", this_source_code.replace("\\", "\\\\").replace("\"", "\\\"").replace("\n", "\\n\\\n")))
